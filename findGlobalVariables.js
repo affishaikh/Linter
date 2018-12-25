2bec59052e33df56128f921f23084b3a25e0c8cd
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2018-12-25 19:12:49 +0530

    Modified core functionality into jq
    Modified following functions:
    findAllVaraiblesAndTheirLineNumbers
    findRangesOfAllFunctions
    Deleted all JSON data generating functions

--- findGlobalVariables.js
+++ findGlobalVariables.js
@@ -1,51 +1,5 @@
 const { execSync } = require('child_process');
 
-const isVariable = function(declaration) {
-  return (
-    declaration['id']['type'] === 'Identifier' &&
-    declaration['init']['type'] === 'Literal'
-  );
-};
-
-const reducer = function(result, declaration) {
-  if (isVariable(declaration)) {
-    let name = declaration['id']['name'];
-    let lineNumber = declaration['id']['loc']['start']['line'];
-    result.push({
-      name,
-      lineNumber
-    });
-  }
-  return result;
-};
-
-const findAllVariablesAndTheirLineNumbers = function(JSONOfDeclarations) {
-  return JSONOfDeclarations.reduce(reducer, []);
-};
-
-const mapper = function(functionDeclaration) {
-  return {
-    start: functionDeclaration['loc']['start']['line'],
-    end: functionDeclaration['loc']['end']['line']
-  };
-};
-
-const findRangesOfAllFunctions = function(JSONOfFunctions) {
-  return JSONOfFunctions.map(mapper);
-};
-
-const generateJSONOfOfVariables = function(fileName) {
-  return JSON.parse(
-    execSync("grasp -j 'var-dec' " + fileName).toString('utf8')
-  );
-};
-
-const generateJSONOfOfFunctions = function(fileName) {
-  return JSON.parse(
-    execSync("grasp -j 'func-exp' " + fileName).toString('utf8')
-  );
-};
-
 const isVariableBetweenLexicalScopeOfAFunction = function(
   rangesOfAllFunctions,
   variable
@@ -65,12 +19,31 @@ const findGlobalVariables = function(allVariables, rangesOfAllFunctions) {
   return allVariables.filter(isVariableBetweenScopeOfAFunction);
 };
 
+const findRangesOfAllFunctions = function(fileName) {
+  return JSON.parse(
+    execSync(
+      "grasp -j 'func-exp' " +
+        fileName +
+        "| jq '[.[] | {'start':.loc.start.line, 'end':.loc.end.line}]'"
+    ).toString('utf8')
+  );
+};
+
+const findAllVariablesAndTheirLineNumbers = function(fileName) {
+  return JSON.parse(
+    execSync(
+      "grasp -j 'var-dec' " +
+        fileName +
+        "| jq '[.[] | {'name':.id.name, 'type':.init.type, 'lineNumber':.init.loc.start.line}]'" +
+        "| jq 'map(select(.type == \"Literal\" or .type == \"ObjectExpression\" or .type == \"ArrayExpression\" or .type == \"ObjectPattern\"))'"
+    ).toString('utf8')
+  );
+};
+
 const main = function() {
   const fileName = process.argv[2];
-  let JSONOfDeclarations = generateJSONOfOfVariables(fileName);
-  let allVariables = findAllVariablesAndTheirLineNumbers(JSONOfDeclarations);
-  let JSONOfFunctions = generateJSONOfOfFunctions(fileName);
-  let rangesOfAllFunctions = findRangesOfAllFunctions(JSONOfFunctions);
+  let allVariables = findAllVariablesAndTheirLineNumbers(fileName);
+  let rangesOfAllFunctions = findRangesOfAllFunctions(fileName);
   let allGlobalVariables = findGlobalVariables(
     allVariables,
     rangesOfAllFunctions
